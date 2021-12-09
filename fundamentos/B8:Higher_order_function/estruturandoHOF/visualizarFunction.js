be89c42b3ffa823c8c92a0818fa919db2f4c32c0
Marcelle Monteiro <monteiro.bio@outlook.com>
2021-12-09 18:15:24 -0300

    Bloco 8: as Higher order function

---
+++ fundamentos/B8:Higher_order_function/estruturandoHOF/visualizarFunction.js
@@ -0,0 +1,5 @@
+const numberGenerator = () => {
+    return Math.random() * 100;
+  }
+  
+  console.log(numberGenerator); //Aqui se imprime somente a escrita, para chamar precisa dos parêmteses.
---
+++ fundamentos/B8:Higher_order_function/first-class/atribuirToVariavel.js
@@ -0,0 +1,12 @@
+function sum (number1, number2) {
+    return number1 + number2;
+  }
+  
+  const sumVariable = sum;
+  
+  console.log(sumVariable);
+  //  [Function: sum]
+
+/*   const sum = (number1, number2) => {
+    return number1 + number2;
+  }; */
