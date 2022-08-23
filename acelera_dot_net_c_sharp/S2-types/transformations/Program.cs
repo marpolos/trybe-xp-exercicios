9f7074e8b9c9fe52744f8286f10ad73850e9ed46
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-08-23 11:25:02 -0300

    feat: conversao de tipos

---
+++ acelera_dot_net_c_sharp/S2-types/transformations/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main()
+    {        
+        int someIntNumber = 51;
+        long longlNumberCast = someIntNumber;
+
+        Console.WriteLine(longlNumberCast);
+
+        // Aqui vai ocorrer uma converssão implicita
+         int intResult = 5/2;
+
+        Console.WriteLine(intResult);
+
+        // Conversão explicita com sintaxe de parênses
+        long someLongNumber = 516144564564654;
+        int intNumber = (int) someLongNumber;
+
+        Console.WriteLine(intNumber);
+
+        // Conversão com Convert.ToInt32()
+        long someLongNumber = 516144564564654;
+        int intNumber = Convert.ToInt32(someLongNumber);
+
+        Console.WriteLine(intNumber);
+    }            
+}
